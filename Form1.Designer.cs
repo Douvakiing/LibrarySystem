c58a416f829211f7d40a185e95d50977117cde43
mina-emad-2099 <mina.emad.2099@gmail.com>
2026-05-06 14:30:48 +0300

    last touches for form 1 main page

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,11 @@
-﻿namespace LibrarySystem
+﻿using System.Windows.Forms;
+
+namespace LibrarySystem
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +17,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.label = new System.Windows.Forms.Label();
@@ -33,79 +24,114 @@
             this.btnManageMembersPage = new System.Windows.Forms.Button();
             this.btnMainDeskPage = new System.Windows.Forms.Button();
             this.btnAdminPanelPage = new System.Windows.Forms.Button();
+            this.tbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            
             // 
             // label
             // 
-            this.label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.label.Location = new System.Drawing.Point(152, 9);
+            this.label.Location = new System.Drawing.Point(152, 20);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(429, 46);
+            this.label.Size = new System.Drawing.Size(429, 100);
             this.label.TabIndex = 0;
-            this.label.Text = "Library Management System";
+            this.label.Text = "Library Management System\n(Team 4)";
             this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            
+            //
+            // tbox (SQL Connection Status)
+            //
+            this.tbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbox.Location = new System.Drawing.Point(216, 140);
+            this.tbox.Name = "tbox";
+            this.tbox.ReadOnly = true;
+            this.tbox.Multiline=true;
+            this.tbox.WordWrap=true;
+            this.tbox.Enabled=false;
+            this.tbox.Size = new System.Drawing.Size(300, 40);
+            this.tbox.TabIndex = 5;
+            this.tbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbox.Text = "Checking SQL connection...";
+            this.tbox.ReadOnly=true;
+            
             // 
             // btnManageBooksPage
             // 
-            this.btnManageBooksPage.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnManageBooksPage.Location = new System.Drawing.Point(34, 222);
+            this.btnManageBooksPage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnManageBooksPage.Location = new System.Drawing.Point(34, 500);
             this.btnManageBooksPage.Name = "btnManageBooksPage";
             this.btnManageBooksPage.Size = new System.Drawing.Size(127, 46);
             this.btnManageBooksPage.TabIndex = 1;
             this.btnManageBooksPage.Text = "Manage Books";
             this.btnManageBooksPage.UseVisualStyleBackColor = true;
             this.btnManageBooksPage.Click += new System.EventHandler(this.btnManageBooksPage_Click_1);
+            
             // 
             // btnManageMembersPage
             // 
-            this.btnManageMembersPage.Location = new System.Drawing.Point(208, 222);
+            this.btnManageMembersPage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnManageMembersPage.Location = new System.Drawing.Point(208, 500);
             this.btnManageMembersPage.Name = "btnManageMembersPage";
             this.btnManageMembersPage.Size = new System.Drawing.Size(127, 46);
             this.btnManageMembersPage.TabIndex = 2;
             this.btnManageMembersPage.Text = "Manage Members";
             this.btnManageMembersPage.UseVisualStyleBackColor = true;
             this.btnManageMembersPage.Click += new System.EventHandler(this.btnManageMembersPage_Click);
+            
             // 
             // btnMainDeskPage
             // 
-            this.btnMainDeskPage.Location = new System.Drawing.Point(383, 222);
+            this.btnMainDeskPage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnMainDeskPage.Location = new System.Drawing.Point(383, 500);
             this.btnMainDeskPage.Name = "btnMainDeskPage";
             this.btnMainDeskPage.Size = new System.Drawing.Size(127, 46);
             this.btnMainDeskPage.TabIndex = 3;
             this.btnMainDeskPage.Text = "Main Desk";
             this.btnMainDeskPage.UseVisualStyleBackColor = true;
+            this.btnMainDeskPage.Click += new System.EventHandler(this.btnMainDeskPage_Click);
+            
             // 
             // btnAdminPanelPage
             // 
-            this.btnAdminPanelPage.Location = new System.Drawing.Point(566, 222);
+            this.btnAdminPanelPage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnAdminPanelPage.Location = new System.Drawing.Point(566, 500);
             this.btnAdminPanelPage.Name = "btnAdminPanelPage";
             this.btnAdminPanelPage.Size = new System.Drawing.Size(127, 46);
             this.btnAdminPanelPage.TabIndex = 4;
             this.btnAdminPanelPage.Text = "Admin Panel";
             this.btnAdminPanelPage.UseVisualStyleBackColor = true;
+            this.btnAdminPanelPage.Click += new System.EventHandler(this.btnAdminPanelPage_Click);
+            
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(717, 412);
-this.btnAdminPanelPage.Click += new System.EventHandler(this.btnAdminPanelPage_Click);
-                        this.Controls.Add(this.btnAdminPanelPage);
-this.btnMainDeskPage.Click += new System.EventHandler(this.btnMainDeskPage_Click);
-                        this.Controls.Add(this.btnMainDeskPage);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 600);
+            
+            // ADDED: Make sure all controls are actually added to the form!
+            this.Controls.Add(this.tbox); 
+            this.Controls.Add(this.btnAdminPanelPage);
+            this.Controls.Add(this.btnMainDeskPage);
             this.Controls.Add(this.btnManageMembersPage);
             this.Controls.Add(this.btnManageBooksPage);
             this.Controls.Add(this.label);
+            
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         #endregion
+        
         private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox tbox;
         private System.Windows.Forms.Button btnManageBooksPage;
         private System.Windows.Forms.Button btnManageMembersPage;
         private System.Windows.Forms.Button btnMainDeskPage;
         private System.Windows.Forms.Button btnAdminPanelPage;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace LibrarySystem
@@ -15,16 +16,50 @@ namespace LibrarySystem
         public Form1()
         {
             InitializeComponent();
-            this.Text="LibrarySystem";
+            this.Text = "LibrarySystem";
+            
+            // Call the connection check as soon as the app starts
+            CheckDatabaseConnection();
+        }
+
+        private void CheckDatabaseConnection()
+        {
+            try
+            {
+                // Attempt to connect to the database
+                using (SqlConnection con = new SqlConnection(Program.ConnectionString))
+                {
+                    con.Open();
+                    
+                    // If we get here, the connection was successful
+                    tbox.Text = "Connected to SQL Server";
+                    tbox.BackColor = Color.LightGreen;
+                }
+            }
+            catch (SqlException ex)
+            {
+                // If it fails, show error status
+                tbox.Text = "SQL Connection Failed .\r\nCannot access rest of operations";
+                tbox.BackColor = Color.LightCoral;
+
+                // Lock down the navigation buttons
+                btnManageBooksPage.Enabled = false;
+                btnManageMembersPage.Enabled = false;
+                btnMainDeskPage.Enabled = false;
+                btnAdminPanelPage.Enabled = false;
+
+                // Show an error pop-up with the exact SQL error
+                MessageBox.Show("Could not connect to the database. The system is locked.\n\nError Details:\n" + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnManageBooksPage_Click_1(object sender, EventArgs e)
         {
-            // This creates the new window and pops it up
             BookManagerForm bookForm = new BookManagerForm();
             bookForm.Show();
             this.Hide();
         }
+        
         private void btnAdminPanelPage_Click(object sender, EventArgs e)
         {
             AdminPanelForm adminpanel = new AdminPanelForm();
@@ -47,4 +82,3 @@ namespace LibrarySystem
         }
     }
 }
-
